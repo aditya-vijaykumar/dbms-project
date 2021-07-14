2a3aff96736e5412f6c96b8579e381a3cf859773
Aditya Vijaykumar <aditya.tarus@gmail.com>
2021-07-14 14:42:30 +0530

    Update Backend Node js app with employee table API

--- backend/index.js
+++ backend/index.js
@@ -7,26 +7,27 @@ const app = express();
 
 const db = mysql.createConnection({
   host: 'localhost',
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  user: 'aditya',
+  password: 'avk123',
   database: 'dbms_project'
 });
 
-// connect to database
 db.connect((err) => {
   if (err) {
     throw err;
   }
   console.log('Connected to database');
-});
+})
 global.db = db;
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.text());
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
--- backend/routes/app.js
+++ backend/routes/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 require("dotenv").config();
 
-router.get('/sailors/all', secured, (req, res) => {
-  let query = "SELECT * FROM `sailors` ORDER BY sid ASC";
+router.get('/employee/all', (req, res) => {
+  let query = "SELECT * FROM employee ORDER BY uid ASC";
   // execute query
   db.query(query, (err, result) => {
     if (err) {
@@ -13,4 +13,73 @@ router.get('/sailors/all', secured, (req, res) => {
   });
 })
 
+router.post('/employee/insert', (req, res) => {
+  console.log(req.body)
+  let Fname = req.body.Fname
+  let Lname = req.body.Lname
+  let ssn = req.body.ssn
+  let bdate = req.body.bdate
+  let sex = req.body.sex
+  let salary = req.body.salary
+  console.log(Fname + " " + Lname + " " + ssn + " " + bdate + " " + sex + " " + salary)
+  let query = `INSERT INTO employee 
+            (
+              Fname, Lname, ssn, bdate, sex, salary
+            )
+            VALUES
+            (
+                ?, ?, ?, ?, ?, ?
+            )`;
+  db.query(query, [Fname, Lname, ssn, bdate, sex, salary], (err, data) => {
+    if (err) {
+      console.error(err)
+      res.status(400).send('Some Error Occured')
+    } else {
+      console.log(data)
+      res.status(201).send({ success: true })
+    }
+  });
+})
+
+router.post('/employee/update/:uid', (req, res) => {
+  let Fname = req.body.Fname
+  let Lname = req.body.Lname
+  let ssn = req.body.ssn
+  let bdate = req.body.bdate
+  let sex = req.body.sex
+  let salary = req.body.salary
+  let uid = req.params.uid
+  let query = `UPDATE employee set 
+  Fname = ?, 
+  Lname = ?, 
+  ssn = ?, 
+  bdate = ?, 
+  sex = ?, 
+  salary = ? 
+  WHERE uid = ?`;
+  db.query(query, [Fname, Lname, ssn, bdate, sex, salary, uid], (err, data) => {
+    if (err) {
+      console.error(err)
+      res.status(400).send('Some Error Occured')
+    } else {
+      console.log(data)
+      res.status(200).send({ success: true })
+    }
+  });
+})
+
+router.post('/employee/delete/:uid', (req, res) => {
+  let uid = req.params.uid
+  console.log(uid)
+  let query = `DELETE FROM employee WHERE uid = ?`
+  db.query(query, uid, (err, data) => {
+    if (err) {
+      console.error(err)
+      res.status(400).send('Some Error Occured')
+    } else {
+      console.log(data)
+      res.status(204).send({ success: true })
+    }
+  });
+})
 module.exports = router;
